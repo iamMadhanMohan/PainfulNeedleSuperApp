b2be332a4cbab397818b9a09c6b27b45f9510856
IamMadhanMohan <mrmadhan09@gmail.com>
2025-03-13 14:47:45 -0400

    Finished Project Folder Setup

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Adam Super App"
+include(":app")
+include(":core")
+include(":feature-learn")
+include(":feature-handyman")
+include(":feature-mechanic")
+include(":feature-pcrepair")
+include(":feature-laundry")
+include(":feature-housecleaning")
