5b49eb637aa0f8d8e3332dfdcc4b1f9f7a8be8ba
Asante Adarkwa Usman <usmanadarkwa@gmail.com>
2025-03-14 17:10:51 -0400

    Added a button in the onboarding screen and added comments

--- app/src/main/java/com/madhan/adamsuperapp/MainActivity.kt
+++ app/src/main/java/com/madhan/adamsuperapp/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import com.madhan.adamsuperapp.ui.theme. AdamSuperAppTheme
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.madhan.feature_hotel.ui.screens.OnboardingScreen
 import dagger.hilt.android.AndroidEntryPoint
--- feature-hotel/src/main/java/com/madhan/feature_hotel/ui/screens/OnboardingScreen.kt
+++ feature-hotel/src/main/java/com/madhan/feature_hotel/ui/screens/OnboardingScreen.kt
@@ -11,29 +11,30 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.madhan.feature_hotel.R
 
 @Composable
 fun OnboardingScreen(paddingValues: PaddingValues) {
+
+    //Custom Colors
     val orange = Color(0xFFFF7A1A)
     val hotelTextColor = Color(0xFF171D33)
     val descriptionColor = Color(0xFFA6AAB4)
@@ -70,6 +71,7 @@ fun OnboardingScreen(paddingValues: PaddingValues) {
                 .aspectRatio(1f)
         )
         Spacer(modifier = Modifier.height(30.dp))
+        //Hotel Text
         Text(
             modifier = Modifier.fillMaxWidth(),
             text="Hotel",
@@ -80,7 +82,8 @@ fun OnboardingScreen(paddingValues: PaddingValues) {
                 color = hotelTextColor
             )
         )
-        Spacer(modifier = Modifier.height(15.dp))
+        Spacer(modifier = Modifier.height(20.dp))
+        //Description
         Text(
             modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp),
             text="Lorem ipsum dolor sit amet, consectetur adipiscing elit.Duis lobortis sit amet odio in egestas. Pellen tesque ultricies justo.",
@@ -93,6 +96,27 @@ fun OnboardingScreen(paddingValues: PaddingValues) {
 
             )
         )
+        Spacer(modifier = Modifier.height(60.dp))
+        //Button
+        Button(
+            onClick = {},
+            enabled = true,
+            colors = ButtonDefaults.buttonColors(orange),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier
+                .width(257.dp)
+                .height(56.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Text(
+                "Let' go",
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.SemiBold,
+                    )
+            )
+        }
 
     }
 }
