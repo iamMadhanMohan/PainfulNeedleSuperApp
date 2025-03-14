c01f4471c15aa549e2c11f59f4be0b1250fb7329
Asante Adarkwa Usman <usmanadarkwa@gmail.com>
2025-03-14 17:14:18 -0400

    Added a button in the onboarding screen and added comments

--- app/src/main/java/com/madhan/adamsuperapp/ui/theme/Theme.kt
+++ app/src/main/java/com/madhan/adamsuperapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.madhan.adamsuperapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
