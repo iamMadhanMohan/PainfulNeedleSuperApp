ad9b08a14310dbad230852f2db0b6f278fb07692
anirudhreddykuchuru99 <anirudhkumarreddy99@gmail.com>
2025-03-19 13:48:44 -0400

    Updated the maps to core module

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -54,4 +56,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.google.maps.android:maps-compose:4.4.0")
 }
